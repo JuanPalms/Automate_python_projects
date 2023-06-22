8f37dd068ad8717852f081461b9bd482aded7591
JuanPalms <palmerosjuanfrancisco@gmail.com>
2023-06-22 12:20:34 -0600

    Ends city weather scripting

--- apis/exercises_apis/weather_apis.py
+++ apis/exercises_apis/weather_apis.py
@@ -5,6 +5,7 @@ an specified city in a span of time of 5 days every 3 hours
 import os
 import sys
 import requests
+import pandas as pd
 
 # Add the parent directory to sys.path
 current_dir = os.getcwd()
@@ -14,7 +15,27 @@ from outils import load_config
 
 config_f = load_config("config.yaml")
 
-r = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q=München,DE&appid={config_f["api_key_weather"]}&units=metric')
-content = r.json()
 
-print(content[0])
+def create_weather_db(city, country_code):
+    """
+    This python module implements a process for building a weather database for the selected city
+    Args:
+    city(string): String that contains the name of the city you want the weather for
+    country(strin): String that contains the code of the country in which the city is located
+    Returns:
+    weather_df(pandas dataframe): Pandas dataframe storing the weather conditions every 3 hours for the chosen city
+    """
+    r = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q={city},{country_code}&appid={config_f["api_key_weather"]}&units=metric')
+    content = r.json()
+    data=[]
+    for element in content['list']:
+        lista = []
+        lista.append(city)
+        lista.append(element['dt_txt'])
+        lista.append(element['main']['temp'])
+        lista.append(element['weather'][0]['description'])
+        data.append(lista)
+    weather_df=pd.DataFrame(data, columns=["City","Time","Temperature","Condition"])
+    return weather_df
+
+create_weather_db('Ciudad de Mexico', 'MEX').to_csv("weather_mx.csv")
