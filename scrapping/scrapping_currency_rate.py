1483d9b4371dfc9a668de1a11084955266eb19ab
JuanPalms <palmerosjuanfrancisco@gmail.com>
2023-06-24 10:49:20 -0600

    Adds shell arguments for script

--- scrapping/scrapping_currency_rate.py
+++ scrapping/scrapping_currency_rate.py
@@ -4,6 +4,13 @@ to get the exchange rate
 """
 import requests
 from bs4 import BeautifulSoup
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('in_cur')
+parser.add_argument('out_cur')
+
+args = parser.parse_args()
 
 def get_currency_rate(in_cur, out_cur):
     url=f'https://www.x-rates.com/calculator/?from={in_cur}&to={out_cur}&amount=1'
@@ -14,4 +21,4 @@ def get_currency_rate(in_cur, out_cur):
     rate = rate.split(" ")[0]
     print(rate)
 
-get_currency_rate('USD','MXN')
+get_currency_rate(args.in_cur,args.out_cur)
