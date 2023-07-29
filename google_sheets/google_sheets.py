a678f814bf48b1ce9719d827e5f8224e8890c3a5
JuanPalms <palmerosjuanfrancisco@gmail.com>
2023-07-28 19:01:12 -0600

    read a public google sheet

---
+++ google_sheets/google_sheets.py
@@ -0,0 +1,8 @@
+"""
+This python module introduces the method to habdling google sheets.
+"""
+import pandas as pd
+url1="https://docs.google.com/spreadsheets/d/1D7U4A9c-hwWWYokmGWAQnUTKsyvEmV9syig8NJuVa84/gviz/tq?tqx=out:csv&sheet=2013"
+url2="https://docs.google.com/spreadsheets/d/1D7U4A9c-hwWWYokmGWAQnUTKsyvEmV9syig8NJuVa84/gviz/tq?tqx=out:csv&sheet=2014"
+data = pd.read_csv(url2)
+print(data)
