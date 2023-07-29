dfa51427ef93e50e2c2a88542f7f66afa5c71e96
JuanPalms <palmerosjuanfrancisco@gmail.com>
2023-07-28 19:16:58 -0600

    Adds google private sheet library

--- google_sheets/private_sheet_google.py
+++ google_sheets/private_sheet_google.py
@@ -6,11 +6,15 @@ import gspread
 # stablish connection with google sheets
 gc = gspread.service_account(filename='google_keys.json')
 
-# Get a spreadsheet by name
+# Get a spreadsheet
 spreadsheet = gc.open("python_exercise")
 # Specify a sheet by its index
 worksheet = spreadsheet.get_worksheet(0)
 
+## get a worksheet by title
+worksheet1 = spreadsheet.worksheet("2013")
+worksheet2 = spreadsheet.worksheet("2014")
+
 # Get the data from the spreadsheet
-data = worksheet.get_all_records()
+data = worksheet2.get_all_records()
 print(data[10])
